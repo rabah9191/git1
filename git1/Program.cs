3733c641702feeea2e891ea5617c69211364343a
Rabah Mahmoud <raba0020@edu.eal.dk>
2018-09-24 13:14:30 +0200

    helper class tilføjet

---
+++ git1/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello git1");  
+            Console.WriteLine("ny ændring");
+            Console.ReadLine();
+
+        }
+    }
+}
